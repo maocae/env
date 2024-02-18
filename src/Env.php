547e3e64f7a910762b01d6a5d9f4a29b9ad9e241
Ritsu Hezayuki <ritsu.hezayuki@gmail.com>
2024-02-18 23:32:30 +0700

    Modify immutable flow and add getAll method

--- src/Env.php
+++ src/Env.php
@@ -2,6 +2,7 @@
 
 namespace Maocae\Env;
 
+use Maocae\Env\Exceptions\ImmutableKeyException;
 use Maocae\Support\Patterns\Singleton;
 
 class Env extends Singleton
@@ -9,10 +10,16 @@ class Env extends Singleton
     /**
      * Prevent any change on any applied environment variable
      *
-     * @var bool $mutable
+     * @var bool $immutable
      */
-    protected bool $mutable = false;
+    protected bool $immutable = false;
 
+    /**
+     * Set whether the new variable gonna stored on $_ENV or putenv
+     *
+     * @var bool $store_in_env_variables
+     */
+    protected bool $store_in_env_variables = true;
     /**
      * Set to true to only return local environment variables (set by the operating system or putenv).
      *
@@ -21,14 +28,25 @@ class Env extends Singleton
     protected bool $local_only = true;
 
     /**
-     * Mutable property setter
+     * Set whether the new variable gonna stored on $_ENV or putenv
      *
-     * @param bool $mutable
+     * @param bool $store_in_env_variables
      * @return void
      */
-    public function setMutable(bool $mutable = true): void
+    public function setStoreInEnvironmentVariables(bool $store_in_env_variables = true): void
     {
-        $this->mutable = $mutable;
+        $this->store_in_env_variables = $store_in_env_variables;
+    }
+
+    /**
+     * immutable property setter
+     *
+     * @param bool $immutable
+     * @return void
+     */
+    public function setImmutable(bool $immutable = true): void
+    {
+        $this->immutable = $immutable;
     }
 
     /**
@@ -63,7 +81,21 @@ class Env extends Singleton
      */
     public function setEnv(string $key, mixed $value = null): void
     {
-        $_ENV[$key] = $value;
+        if ($this->immutable) {
+            throw new ImmutableKeyException();
+        }
+
+        $this->store_in_env_variables ? putenv("$key=$value") : $_ENV[$key] = $value;
+    }
+
+    /**
+     * Get all available environment value
+     *
+     * @return array
+     */
+    public function getAll(): array
+    {
+        return array_merge(getenv(), $_ENV);
     }
 
     /**
--- src/Facades/Env.php
+++ src/Facades/Env.php
@@ -6,11 +6,13 @@ use Maocae\Env\Env as BaseClass;
 use Maocae\Support\Patterns\Traits\CallStatically;
 
 /**
- * @method static void setMutable(bool $mutable = true)
+ * @method static void setImmutable(bool $immutable = true)
  * @method static void setLocalOnly(bool $local_only = true)
+ * @method static void setStoreInEnvironmentVariables(bool $store_in_env_variables = true)
  * @method static mixed getEnv(string $key, mixed $fallback = null)
  * @method static void setEnv(string $key, mixed $value = null)
  * @method static bool removeEnv(string $key)
+ * @method static array getAll()
  */
 class Env
 {
